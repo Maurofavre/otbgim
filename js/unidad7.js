39292abf23b7214a9955d88634988f2b4a909977
MAURO FAVRE <mauro_favre@hotmail.com>
2022-11-15 22:22:33 -0300

    otb

---
+++ js/unidad7.js
@@ -0,0 +1,7 @@
+var nombreUsuario = prompt("¿Cual es tu nombre?");
+var edad = prompt("Bienvenido " + nombreUsuario + " ¿Sos mayor de edad?");
+var direccion = prompt("¿Como nos conosciste?");
+var enfermedades = prompt(
+  nombreUsuario + "¿ Tienes algun Historial medico de interes?"
+);
+var interes = prompt("¿En que clases les gustaria participar?");
